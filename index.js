f9a47a26084d117040a66fea2e50f28cdc8e1e31
Ruben Laban <r.laban@treehouse.nl>
2021-04-29 15:25:24 +0200

    Again

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const tf_setup = require('setup-terraform/lib');
+const tf_setup = require('setup-terraform/lib/setup-terraform');
 
 try {
   const terraformVersion = core.getInput('terraform_version');
