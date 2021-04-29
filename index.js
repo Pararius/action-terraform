8d331527a9c2359b1dd1a20fc666753ed9d501c2
Ruben Laban <r.laban@treehouse.nl>
2021-04-29 16:03:03 +0200

    Await

--- index.js
+++ index.js
@@ -3,19 +3,10 @@ const github = require('@actions/github');
 const tf_setup = require('setup-terraform/lib/setup-terraform');
 const { spawnSync } = require( 'child_process' );
 
-function sleep (time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
-}
-
-try {
+(async () => {
   const terraformVersion = core.getInput('terraform_version');
   tf_setup();
-
   const tf = spawnSync('terraform', ['version']);
-  sleep(2000).then(() => {
-    core.info(`tf1 ${terraformVersion}: ${tf.stdout.toString()}`);
-  });
-core.info(`tf2 ${terraformVersion}: ${tf.stdout.toString()}`);
-} catch (error) {
+})().catch(error => {
   core.setFailed(error.message);
-}
+});
