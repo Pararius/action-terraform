c0cbda8a520a2dc968f542e7690d56516414aedd
Ruben Laban <r.laban@treehouse.nl>
2021-04-29 15:12:02 +0200

    Fix import?

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const tf_setup = require('@hashicorp/setup-terraform#v1');
+const tf_setup = require('hashicorp/setup-terraform#v1');
 
 try {
   const terraformVersion = core.getInput('terraform_version');
