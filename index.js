d671418801f0735b470235d757581d9dfe0f7102
Ruben Laban <r.laban@treehouse.nl>
2021-04-29 15:18:38 +0200

    Doubt this will work either

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const tf_setup = require('setup-terraform#v1/lib');
+const tf_setup = require('setup-terraform/lib');
 
 try {
   const terraformVersion = core.getInput('terraform_version');
