3ca65eb2de1ece32d2bcd5f66523ad6344fc36e5
Ruben Laban <r.laban@treehouse.nl>
2021-04-30 11:21:51 +0200

    abstract

--- dist/index.js
+++ dist/index.js
@@ -16,7 +16,12 @@ function terraform(params) {
   const options = {
     cwd: core.getInput('terraform_directory')
   }
-  return spawnSync('terraform', params, options);
+  const process = spawnSync('terraform', params, options);
+  return {
+    stdout: process.stdout.toString(),
+    stderr: process.stderr.toString(),
+    status: process.status
+  }
 }
 
 (async () => {
@@ -26,24 +31,24 @@ function terraform(params) {
   await tf_setup();
   const tfv = terraform(['version']);
   core.info(`Expected Terraform version: ${terraformVersion}`);
-  core.info(`Actual Terraform version: ${tfv.stdout.toString()}`);
+  core.info(`Actual Terraform version: ${tfv.stdout}`);
   core.info(`Working directory: ${terraformDirectory}`);
   core.endGroup();
   core.startGroup('terraform init');
   const tfi = terraform(['init']);
-  core.info(tfi.stdout.toString());
-  core.info(tfi.stderr.toString());
+  core.info(tfi.stdout);
+  core.info(tfi.stderr);
   tfi
   core.endGroup();
   core.startGroup('terraform fmt');
   const tff = terraform(['fmt', '-diff', '-write=false', '-list=false']);
-  core.info(tff.stdout.toString());
-  core.info(tff.stderr.toString());
+  core.info(tff.stdout);
+  core.info(tff.stderr);
   core.endGroup();
   core.startGroup('terraform plan');
   const tfp = terraform(['plan']);
-  core.info(tfp.stdout.toString());
-  core.info(tfp.stderr.toString());
+  core.info(tfp.stdout);
+  core.info(tfp.stderr);
   core.endGroup();
 })().catch(error => {
   core.setFailed(error.message);
