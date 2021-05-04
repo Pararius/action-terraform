0fc1c6180d7d8af8d392a0ee3915f8a06da18ee7
Ruben Laban <r.laban@treehouse.nl>
2021-05-04 14:10:06 +0200

    update & build

--- dist/index.js
+++ dist/index.js
@@ -60,7 +60,7 @@ function terraform(params) {
   core.info(tfs.stderr);
   core.endGroup();
   if (tfs.status > 0) {
-    core.setFailed('Failed to determine which terraform version to use');
+    core.setFailed(`Failed to determine which terraform version to use [err:${tfs.status}]`);
     process.exit(1);
   }
 
@@ -68,61 +68,65 @@ function terraform(params) {
   const tfv = terraform('version');
   core.info(tfv.stdout);
   core.info(tfv.stderr);
+  core.endGroup();
   if (tfv.status > 0) {
     core.info(`status: ${tfv.status}`);
+    core.setFailed(`Failed to determine terraform version [err:${tfv.status}]`);
   } else {
     tf_version = tfv.stdout.replace(/\r?\n|\r/g, ' ').match(/ v([0-9]+\.[0-9]+\.[0-9]+) /)[1];
   }
-  core.endGroup();
 
   core.startGroup('terraform init');
   const tfi = terraform('init');
+  core.info(tfi.stdout);
+  core.endGroup();
   if (tfi.status > 0) {
-    core.info(`status: ${tfi.status}`);
     tf_init = `\u2715`;
+    core.setFailed(`Failed to initialize terraform [err:${tfi.status}]`);
   } else {
     tf_init = `\u2713`;
   }
-  core.info(tfi.stdout);
-  core.endGroup();
 
   core.startGroup('terraform fmt');
   const tffc = terraform('fmt -check');
   if (tffc.status > 0) {
-    core.info(`status: ${tffc.status}`);
-    tf_fmt = `\u2715`;
     const tff = terraform('fmt -diff -write=false -list=false');
     core.info(tff.stdout);
+  }
+  core.endGroup();
+  if (tffc.status > 0) {
+    tf_fmt = `\u2715`;
+    core.setFailed(`Failed to pass terraform formatting checks [err:${tffc.status}]`);
   } else {
     tf_fmt = `\u2713`;
   }
-  core.endGroup();
 
   core.startGroup('terraform plan');
   const tfp = terraform('plan -out=terraform.plan');
+  core.info(tfp.stdout);
+  core.endGroup();
   if (tfp.status > 0) {
-    core.info(`status: ${tfp.status}`);
     tf_plan = `\u2715`;
+    core.setFailed(`Failed to prepare the terraform plan [err:${tfp.status}]`);
   } else {
     tf_plan = `\u2713`;
   }
-  core.info(tfp.stdout);
-  core.endGroup();
 
   core.startGroup('terraform apply');
   if (terraformDoApply === 'true') {
     const tfa = terraform('apply -auto-approve terraform.plan');
+    core.info(tfa.stdout);
+    core.endGroup();
     if (tfa.status > 0) {
-      core.info(`status: ${tfa.status}`);
       tf_apply = `\u2715`;
+      core.setFailed(`Failed to apply terraform plan [err:${tfa.status}]`);
     } else {
       tf_apply = `\u2713`;
     }
-    core.info(tfa.stdout);
   } else {
     core.info('Skipped');
+    core.endGroup();
   }
-  core.endGroup();
 
   core.info('');
   core.info(`Version: ${tf_version}`);
