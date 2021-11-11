d1fbcf96952ec0a860123e509228de1a982f3950
Cas Leentfaar <info@casleentfaar.com>
2021-11-11 14:22:04 +0100

    fixed compile

--- dist/index.js
+++ dist/index.js
@@ -720,7 +720,7 @@ exports.runTerraformSwitcher = function (terraformDirectory) {
 }
 
 exports.setVariable = function (key, value) {
-    return exports.runCommand(`printf '%s' '${key}' > ${value}`);
+    process.env[key] = value
 }
 
 
